a24e9e5b4dafd245d4ec004acd02fb64a815daa0
ilovegithubsomuchh <49435669+ilovegithubsomuchh@users.noreply.github.com>
2024-02-14 12:27:23 +0100

    Push

---
+++ Assets/Scripts/DragAndDrop.cs
@@ -0,0 +1,58 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+public class DragAndDrop : MonoBehaviour, IPointerDownHandler, IBeginDragHandler,IEndDragHandler, IDragHandler, IPointerUpHandler
+{
+
+
+    private RectTransform _rectTransform;
+    private BoxCollider2D _collider2D;
+    private float scrollInput;
+
+
+    private void Update()
+    {
+        float scrollInput = Input.GetAxis("Mouse ScrollWheel");
+        transform.Rotate(Vector3.up, scrollInput * 10 * Time.deltaTime);
+    }
+
+    private void Awake()
+    {
+        _rectTransform = GetComponent<RectTransform>();
+        _collider2D = GetComponent<BoxCollider2D>();
+    }
+
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        _rectTransform.anchoredPosition += eventData.delta;
+        _collider2D.enabled = false;
+    }
+    
+
+    public void OnEndDrag(PointerEventData eventData)
+    {
+        Debug.Log("onEndDrag");
+    }
+
+   
+
+    public void OnBeginDrag(PointerEventData eventData)
+    {
+        Debug.Log("OnBeginDrag");
+    }
+
+    public void OnDrag(PointerEventData eventData)
+    {
+        _rectTransform.anchoredPosition += eventData.delta;
+        Debug.Log("OnDrag");
+    }
+
+    public void OnPointerUp(PointerEventData eventData)
+    {
+        Debug.Log("onPointerUp");
+        _collider2D.enabled = true;
+    }
+    
+}
---
+++ Assets/Scripts/RotateRoom.cs
@@ -0,0 +1,23 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateRoom : MonoBehaviour
+{
+    public GameObject Room;
+    private float rotation = 0f;
+    
+    // Start is called before the first frame update
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+       
+  
+        if (other.CompareTag("Player"))
+        {
+            rotation -= 90f;
+            Room.transform.rotation = Quaternion.Euler(0,0,rotation);
+        }
+    }
+}
