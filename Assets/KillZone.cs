683087f31a5d5d56584d8fe04234f368cfe5003b
ilovegithubsomuchh <49435669+ilovegithubsomuchh@users.noreply.github.com>
2024-02-14 16:49:59 +0100

    KillZone et le rangement t'as peur

---
+++ Assets/KillZone.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class KillZone : MonoBehaviour
+{
+    public int Respawn;
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            Destroy(other.gameObject);
+            SceneManager.LoadScene(Respawn);
+        }
+    }
+}
